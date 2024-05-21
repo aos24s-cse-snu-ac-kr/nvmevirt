393ea7bf2b6aded8cc7adc78fc52525b2e57553f
haeramkeem <haeram.kim1@gmail.com>
2024-05-21 12:30:07 +0000

    feat: comment nvme_proc_dbs description

--- main.c
+++ main.c
@@ -122,22 +122,23 @@ static bool nvmev_proc_dbs(void)
 	bool updated = false;
 
 	// Admin queue
+  // - Submission queue: Doorbell index [0]
 	new_db = nvmev_vdev->dbs[0];
 	if (new_db != nvmev_vdev->old_dbs[0]) {
-        //NVMEV_DEBUG_TRACE(&nvmev_proc_dbs);
 		nvmev_proc_admin_sq(new_db, nvmev_vdev->old_dbs[0]);
 		nvmev_vdev->old_dbs[0] = new_db;
 		updated = true;
 	}
+  // - Completion queue: Doorbell index [1]
 	new_db = nvmev_vdev->dbs[1];
 	if (new_db != nvmev_vdev->old_dbs[1]) {
-        //NVMEV_DEBUG_TRACE(&nvmev_proc_dbs);
 		nvmev_proc_admin_cq(new_db, nvmev_vdev->old_dbs[1]);
 		nvmev_vdev->old_dbs[1] = new_db;
 		updated = true;
 	}
 
-	// Submission queues
+  // IO queue
+	// - Submission queues: Doorbell index [even]
 	for (qid = 1; qid <= nvmev_vdev->nr_sq; qid++) {
 		if (nvmev_vdev->sqes[qid] == NULL)
 			continue;
@@ -145,13 +146,12 @@ static bool nvmev_proc_dbs(void)
 		new_db = nvmev_vdev->dbs[dbs_idx];
 		old_db = nvmev_vdev->old_dbs[dbs_idx];
 		if (new_db != old_db) {
-            //NVMEV_DEBUG_TRACE(&nvmev_proc_dbs);
 			nvmev_vdev->old_dbs[dbs_idx] = nvmev_proc_io_sq(qid, new_db, old_db);
 			updated = true;
 		}
 	}
 
-	// Completion queues
+	// - Completion queues: Doorbell index [odd]
 	for (qid = 1; qid <= nvmev_vdev->nr_cq; qid++) {
 		if (nvmev_vdev->cqes[qid] == NULL)
 			continue;
@@ -159,7 +159,6 @@ static bool nvmev_proc_dbs(void)
 		new_db = nvmev_vdev->dbs[dbs_idx];
 		old_db = nvmev_vdev->old_dbs[dbs_idx];
 		if (new_db != old_db) {
-            //NVMEV_DEBUG_TRACE(&nvmev_proc_dbs);
 			nvmev_proc_io_cq(qid, new_db, old_db);
 			nvmev_vdev->old_dbs[dbs_idx] = new_db;
 			updated = true;
